1447ba4df21ecb5218f8ba4a038078829cdd5dda
Shohei Yamasaki <shyamasaki@gmail.com>
2013-07-29 11:32:07 +0900

    .tmux.confをsetup.shでセットする

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .bashrc .gitconfig .zshrc .gemrc)
+DOT_FILES=(.vimrc .bashrc .gitconfig .zshrc .gemrc .tmux.conf)
 
 for file in ${DOT_FILES[@]}; do
     ln -s $HOME/dotfiles/$file $HOME/$file
