80f1b993f41fbcb2642ea6db155f47082b2efdfc
Shohei Yamasaki <shyamasaki@gmail.com>
2013-08-07 05:57:02 +0900

    setup.shにpathogenいれる

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .bashrc .gitconfig .zshrc .gemrc .tmux.conf .oh-my-zsh)
+DOT_FILES=(.vim .vimrc .bashrc .gitconfig .zshrc .gemrc .tmux.conf .oh-my-zsh)
 
 for file in ${DOT_FILES[@]}; do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
