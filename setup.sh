ed095aa7f9b8e7c0ead2791d62a1f1edc248bce6
Shohei Yamasaki <shyamasaki@gmail.com>
2013-08-07 05:36:47 +0900

    setup.shにoh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .bashrc .gitconfig .zshrc .gemrc .tmux.conf)
+DOT_FILES=(.vimrc .bashrc .gitconfig .zshrc .gemrc .tmux.conf .oh-my-zsh)
 
 for file in ${DOT_FILES[@]}; do
     ln -s $HOME/dotfiles/$file $HOME/$file
