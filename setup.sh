5c5b9aeed9c399e26dfac20a6ce168df5e7d9e27
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-11-07 23:30:59 +0900

    update

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ for file in ${DOT_FILES[@]}; do
 done
 
 if [ ! -x /usr/local/bin/brew ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
